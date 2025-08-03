180b191c6656f203527029a57f987d5a88ef9e8c
cottoncammy <110693401+cottoncammy@users.noreply.github.com>
2025-08-03 01:37:56 -0400

    Replace terminal, switch to updated clangd

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" || goto :error
 set LLVM=C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/Llvm/x64/bin
 cmake -S . -B build -G Ninja -DCMAKE_C_COMPILER:FILEPATH="%LLVM%/clang-cl.exe" -DCMAKE_LINKER_TYPE=LLD || goto :error
 cmake --build build || goto :error
