6c517444f1a92f2f069394a0e5e0ce9579efb51b
cottoncammy <110693401+cottoncammy@users.noreply.github.com>
2025-08-03 19:52:38 -0400

    Use MSVC Ninja

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
-set LLVM=C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/VC/Tools/Llvm/x64/bin
-cmake -S . -B build -G Ninja -DCMAKE_C_COMPILER:FILEPATH="%LLVM%/clang-cl.exe" -DCMAKE_LINKER_TYPE=LLD || goto :error
+set MSVC=%ProgramFiles(x86)%/Microsoft Visual Studio/2022/BuildTools
+cmake -S . -B build -G Ninja -DCMAKE_PROGRAM_PATH="%MSVC%/Common7/IDE/CommonExtensions/Microsoft/CMake/Ninja" -DCMAKE_C_COMPILER:FILEPATH="%MSVC%/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_LINKER_TYPE=LLD || goto :error
 cmake --build build || goto :error
-cmake --install build --prefix C:/Users/Cameron/Code/engine/target || goto :error
+cmake --install build --prefix "%UserProfile%/Code/engine/target" || goto :error
 
 :error
 exit /b %errorlevel%
