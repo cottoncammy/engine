5bea5dbfd0e7fb3a0dd6a3f75d3fdab37e50114d
cottoncammy <110693401+cottoncammy@users.noreply.github.com>
2025-08-03 17:54:33 -0400

    Tweaks to the build

--- cmake/FindSDL3.cmake
+++ cmake/FindSDL3.cmake
@@ -1,39 +1,94 @@
-find_package(PkgConfig QUIET)
+function(find_sdl3_library out_var)
+    find_library(${out_var}
+        NAMES SDL3-static.a SDL3-static.lib
+        OPTIONAL
+        NO_CMAKE_ENVIRONMENT_PATH
+        NO_SYSTEM_ENVIRONMENT_PATH
+    )
+endfunction()
 
-if(PKG_CONFIG_FOUND)
-    if(SDL3_FIND_QUIETLY)
-        set(PC_FIND_QUIET QUIET)
+if(SDL3_FIND_QUIETLY)
+    set(SDL3_FIND_QUIET QUIET)
+endif()
+
+# on non-Windows, first try pkg-config
+if(NOT CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    find_package(PkgConfig QUIET)
+
+    if(PKG_CONFIG_FOUND)
+        if(SDL3_FIND_VERSION_EXACT)
+            set(PC_FIND_MODULESPEC SDL3=${SDL3_FIND_VERSION})
+        else()
+            set(PC_FIND_MODULESPEC SDL3>=${SDL3_FIND_VERSION)
+        endif()
+
+        pkg_check_modules(SDL3 ${SDL3_FIND_QUIET} ${PC_FIND_MODULESPEC})
+
+        if(SDL3_FOUND)
+            # make sure the package found has SDL3-static.lib
+            set(SDL3_ROOT "${SDL3_LIBDIR}")
+            find_sdl3_library(SDL3_FOUND)
+
+            if(SDL3_FOUND)
+                add_library(SDL3::SDL3 UNKNOWN IMPORTED)
+                set_target_properties(SDL3::SDL3 PROPERTIES
+                    IMPORTED_LOCATION "${SDL3_LIBDIR}"
+                    INTERFACE_INCLUDE_DIRECTORIES "${SDL3_INCLUDE_DIRS}"
+                    INTERFACE_LINK_LIBRARIES "${SDL3_LINK_LIBRARIES}"
+                    IMPORTED_LINK_INTERFACE_LANGUAGES C;CXX
+                    IMPORTED_SONAME "${SDL3_FIND_VERSION_MAJOR}"
+                )
+            else()
+                unset(SDL3_LIBDIR CACHE)
+                unset(SDL3_VERSION CACHE)
+            endif()
+        endif()
+
+        unset(SDL3_ROOT)
+        unset(PC_FIND_MODULE_SPEC)
     endif()
+endif()
 
+# next, try to search for the package in Config mode
+if(NOT SDL3_FOUND)
     if(SDL3_FIND_VERSION_EXACT)
-        set(PC_FIND_MODULE_SPEC_SUFFIX =${SDL3_FIND_VERSION})
-    else()
-        set(PC_FIND_MODULE_SPEC_SUFFIX >=${SDL3_FIND_VERSION)
+        set(SDL3_FIND_EXACT EXACT)
     endif()
 
-    pkg_check_modules(SDL3 ${PC_FIND_QUIET} SDL3${PC_FIND_MODULE_SPEC_SUFFIX})
+    set(SDL3_ROOT "${PROJECT_BINARY_DIR}/vendor/SDL")
 
-    if(SDL3_FOUND)
-        add_library(SDL3::SDL3 UNKNOWN IMPORTED)
-        set_target_properties(SDL3::SDL3 PROPERTIES
-            IMPORTED_LOCATION "${SDL3_LIBDIR}"
-            INTERFACE_INCLUDE_DIRECTORIES "${SDL3_INCLUDE_DIRS}"
-            INTERFACE_LINK_LIBRARIES "${SDL3_LINK_LIBRARIES}"
-            IMPORTED_LINK_INTERFACE_LANGUAGES C;CXX
-            IMPORTED_IMPLIB "${SDL3_LIBDIR}"
-            IMPORTED_SONAME "${SDL3_FIND_VERSION_MAJOR}"
-        )
+    find_package(SDL3
+        ${SDL3_FIND_VERSION}
+        ${SDL3_FIND_EXACT}
+        ${SDL3_FIND_QUIET}
+        CONFIG
+        NO_CMAKE_ENVIRONMENT_PATH
+        NO_SYSTEM_ENVIRONMENT_PATH
+    )
 
-        mark_as_advanced(FORCE SDL3_LIBDIR SDL3_VERSION)
+    if (SDL3_FOUND)
+        # make sure the package found has SDL3-static.lib
+        find_sdl3_library(SDL3_FOUND)
+
+        if (SDL3_FOUND)
+            set(SDL3_LIBDIR "${SDL3_BINARY_DIR}")
+            set(SDL3_VERSION ${SDL3_PROJECT_VERSION})
+        endif()
     endif()
-elseif(SDL3_FIND_REQUIRED)
+
+    unset(SDL3_ROOT)
+    unset(SDL3_FIND_EXACT)
+endif()
+
+# finally, just build it from source
+if(NOT SDL3_FOUND AND SDL3_FIND_REQUIRED)
     set(SDL_DEPS_SHARED OFF)
     set(SDL_SHARED OFF)
     set(SDL_TEST_LIBRARY OFF)
     add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL" EXCLUDE_FROM_ALL)
 
+    set(SDL3_LIBDIR "${SDL3_BINARY_DIR}")
     set(SDL3_VERSION ${SDL3_PROJECT_VERSION})
-    set(SDL3_LIBDIR ${SDL3_BINARY_DIR})
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -41,3 +96,5 @@ find_package_handle_standard_args(SDL3
     REQUIRED_VARS SDL3_LIBDIR
     VERSION_VAR SDL3_VERSION
 )
+
+unset(SDL3_FIND_QUIET)
