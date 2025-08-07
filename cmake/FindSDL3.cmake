2494461d15404fe09b849c541fe32a1276a980e1
cottoncammy <110693401+cottoncammy@users.noreply.github.com>
2025-08-06 23:02:36 -0400

    simplify finding SDL3 (for now)

--- cmake/FindSDL3.cmake
+++ cmake/FindSDL3.cmake
@@ -1,57 +1,40 @@
-function(find_sdl3_library OUT_VAR)
-    find_library(${OUT_VAR}
-        NAMES SDL3-static.a SDL3-static.lib
-        OPTIONAL
-        NO_CMAKE_ENVIRONMENT_PATH
-        NO_SYSTEM_ENVIRONMENT_PATH
-    )
-endfunction()
-
 if(SDL3_FIND_QUIETLY)
     set(SDL3_FIND_QUIET QUIET)
 endif()
 
-# next, try to search for the package in Config mode
-if(NOT SDL3_FOUND)
-    if(SDL3_FIND_VERSION_EXACT)
-        set(SDL3_FIND_EXACT EXACT)
-    endif()
-
-    find_package(SDL3
-        ${SDL3_FIND_VERSION}
-        ${SDL3_FIND_EXACT}
-        ${SDL3_FIND_QUIET}
-        CONFIG
-        NO_CMAKE_ENVIRONMENT_PATH
-        NO_SYSTEM_ENVIRONMENT_PATH
-    )
-    unset(SDL3_FIND_EXACT)
-    unset(SDL3_FIND_QUIET)
-
-    if (SDL3_FOUND)
-        # make sure the package found has SDL3-static.lib
-        set(SDL3_ROOT "${PROJECT_BINARY_DIR}/vendor/SDL")
-        find_sdl3_library(SDL3_FOUND)
-        unset(SDL3_ROOT)
-    endif()
+if(SDL3_FIND_VERSION_EXACT)
+    set(SDL3_FIND_EXACT EXACT)
 endif()
 
-# finally, just build it from source
+# try to search for the package in Config mode
+find_package(SDL3
+    ${SDL3_FIND_VERSION}
+    ${SDL3_FIND_EXACT}
+    ${SDL3_FIND_QUIET}
+    CONFIG
+    NO_CMAKE_ENVIRONMENT_PATH
+    NO_SYSTEM_ENVIRONMENT_PATH
+)
+unset(SDL3_FIND_EXACT)
+unset(SDL3_FIND_QUIET)
+
+# just build it from source
 if(NOT SDL3_FOUND AND SDL3_FIND_REQUIRED)
     set(SDL_DEPS_SHARED OFF)
     set(SDL_SHARED OFF)
     set(SDL_TEST_LIBRARY OFF)
     add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/SDL" EXCLUDE_FROM_ALL)
-    set(SDL3_FOUND TRUE)
-endif()
 
-if(SDL3_FOUND)
-    set(SDL3_LIBDIR "${SDL3_BINARY_DIR}")
-    set(SDL3_VERSION ${SDL3_PROJECT_VERSION})
+    set(SDL3_FOUND TRUE)
+    set(SDL3_INSTALL_PATH "${SDL3_BINARY_DIR}")
+    set(SDL3_VERSION 3.2.20)
+elseif(SDL3_FOUND)
+    get_target_property(SDL3_INSTALL_PATH ${SDL3_LIBRARIES} LOCATION)
+    set(SDL3_VERSION ${PACKAGE_VERSION})
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SDL3
-    REQUIRED_VARS SDL3_LIBDIR
+    REQUIRED_VARS SDL3_INSTALL_PATH
     VERSION_VAR SDL3_VERSION
 )
