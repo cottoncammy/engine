86831de7d0cf2f06e1759fbd63105c5eedebc1b9
cottoncammy <110693401+cottoncammy@users.noreply.github.com>
2025-08-10 16:04:10 -0400

    proper include directories

--- cmake/build-wuffs.cmake
+++ cmake/build-wuffs.cmake
@@ -1,29 +1,38 @@
-include(GNUInstallDirs)
-set(WUFFS_INCLUDE_PATH "${CMAKE_INSTALL_FULL_INCLUDEDIR}/wuffs")
+set(WUFFS_INSTALL_PATH "${CMAKE_CURRENT_BINARY_DIR}/vendor/wuffs")
 
 if(CMAKE_CROSSCOMPILING)
+    include(GNUInstallDirs)
     set(WUFFS_BIN_PATH "${CMAKE_STAGING_PREFIX}/${CMAKE_INSTALL_BINDIR}")
 else()
-    set(WUFFS_BIN_PATH "${CMAKE_CURRENT_BINARY_DIR}/vendor/wuffs")
+    set(WUFFS_BIN_PATH "${WUFFS_INSTALL_PATH}")
 endif()
 
 configure_file(
     "${CMAKE_CURRENT_SOURCE_DIR}/vendor/wuffs/release/c/wuffs-v0.3.c"
-    "${WUFFS_INCLUDE_PATH}/wuffs-base.c"
+    "${WUFFS_INSTALL_PATH}/wuffs-base.c"
     NO_SOURCE_PERMISSIONS
     COPYONLY
 )
 
 add_library(wuffs_base INTERFACE)
+set(WUFFS_BASE_DIRS
+    "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}>"
+    $<INSTALL_INTERFACE:include/wuffs>
+)
+
 target_sources(wuffs_base
     INTERFACE
         FILE_SET wuffs_base_header
         TYPE HEADERS
-        BASE_DIRS "${WUFFS_INCLUDE_PATH}"
+        BASE_DIRS ${WUFFS_BASE_DIRS}
+            "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}>"
+            $<INSTALL_INTERFACE:include/wuffs>
         FILES
-            "${WUFFS_INCLUDE_PATH}/wuffs-base.c"
+            "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-base.c>"
+            $<INSTALL_INTERFACE:include/wuffs/wuffs-base.c>
     INTERFACE
-        "${WUFFS_INCLUDE_PATH}/wuffs-base.c"
+        "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-base.c>"
+        $<INSTALL_INTERFACE:include/wuffs/wuffs-base.c>
 )
 
 find_program(GO_BIN go REQUIRED)
@@ -40,9 +49,9 @@ set(WUFFS_FOO_SRC_FILES
 )
 list(TRANSFORM WUFFS_FOO_SRC_FILES PREPEND "${CMAKE_CURRENT_SOURCE_DIR}/")
 add_custom_command(COMMAND
-    wuffs-c gen -package_name foo ${WUFFS_FOO_SRC_FILES} > "${WUFFS_INCLUDE_PATH}/wuffs-foo.c"
-    DEPENDS "${WUFFS_INCLUDE_PATH}/wuffs-base.c" "${WUFFS_BIN_PATH}/wuffs-c.exe"
-    OUTPUT "${WUFFS_INCLUDE_PATH}/wuffs-foo.c"
+    wuffs-c gen -package_name foo ${WUFFS_FOO_SRC_FILES} > "${WUFFS_INSTALL_PATH}/wuffs-foo.c"
+    DEPENDS ${WUFFS_FOO_SRC_FILES} "${WUFFS_INSTALL_PATH}/wuffs-base.c" "${WUFFS_BIN_PATH}/wuffs-c.exe"
+    OUTPUT "${WUFFS_INSTALL_PATH}/wuffs-foo.c"
     WORKING_DIRECTORY "${WUFFS_BIN_PATH}"
     COMMENT "TODO"
     VERBATIM
@@ -57,11 +66,17 @@ target_sources(wuffs_foo
     INTERFACE
         FILE_SET wuffs_foo_headers
         TYPE HEADERS
-        BASE_DIRS "${WUFFS_INCLUDE_PATH}"
+        BASE_DIRS
+            "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}>"
+            $<INSTALL_INTERFACE:include/wuffs>
         FILES
-            "${WUFFS_INCLUDE_PATH}/wuffs-base.c"
-            "${WUFFS_INCLUDE_PATH}/wuffs-foo.c"
+            "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-base.c>"
+            $<INSTALL_INTERFACE:include/wuffs/wuffs-base.c>
+            "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-foo.c>"
+            $<INSTALL_INTERFACE:include/wuffs/wuffs-foo.c>
     INTERFACE
-        "${WUFFS_INCLUDE_PATH}/wuffs-base.c"
-        "${WUFFS_INCLUDE_PATH}/wuffs-foo.c"
+        "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-base.c>"
+        $<INSTALL_INTERFACE:include/wuffs/wuffs-base.c>
+        "$<BUILD_INTERFACE:${WUFFS_INSTALL_PATH}/wuffs-foo.c>"
+        $<INSTALL_INTERFACE:include/wuffs/wuffs-foo.c>
 )
